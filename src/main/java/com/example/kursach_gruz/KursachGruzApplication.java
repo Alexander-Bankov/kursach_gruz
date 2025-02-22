a7bc481862c8cfcdaa9a25deb56b3008ee4c7044
Alexander-Bankov <1bankov20032mail.ru>
2025-02-23 01:11:24 +0400

    first commit

---
+++ src/main/java/com/example/kursach_gruz/KursachGruzApplication.java
@@ -0,0 +1,13 @@
+package com.example.kursach_gruz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursachGruzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursachGruzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kursach_gruz/KursachGruzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursach_gruz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursachGruzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
